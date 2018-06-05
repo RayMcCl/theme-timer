6cdf4c5fc183761e429ae2376666cfc17c391290
Ray McClain <rmcclain@decibelinsight.com>
2018-06-04 21:07:58 -0600

    Removed console messages

--- src/extension.ts
+++ src/extension.ts
@@ -16,8 +16,6 @@ function updateSettings () {
     const config = vscode.workspace.getConfiguration('theme-timer');
     themeList = config.themes;
     state = config.enable;
-
-    console.log('Config', config);
     
     if(state){
         scheduleTasks();
@@ -29,8 +27,6 @@ function updateSettings () {
 function updateTheme () {
     CONFIG.update(THEME, this.theme, true);
     CONFIG = vscode.workspace.getConfiguration();
-
-    console.log('Update theme', this.theme);
 }
 
 function scheduleTasks () {
